f4e3542402ae83547104a96df0ae4d7aa70c8504
wutong <1772364946@qq.com>
2023-01-10 01:50:11 +0800

    蛮力法(brute force method)

---
+++ src/edu/蛮力法/A5_选择排序.java
@@ -0,0 +1,30 @@
+package edu.蛮力法;
+
+/**
+ * @Author WesterlyWindWuTong
+ * @Date 2023/1/10 01:35
+ * @Version 1.0
+ * @Description:
+ */
+public class A5_选择排序 {
+    public static void main(String[] args) {
+        int[] r ={1,6,7,5,3,8,9,2};
+        SelectSort(r,8);
+        for(int i=0;i<8;i++)
+            System.out.print(r[i]+"  ");
+        System.out.println();
+    }
+    private static void SelectSort(int r[ ], int n)
+    {
+        int i, j, index, temp;
+        for (i = 0; i < n - 1; i++)  	            //对n个记录进行n-1趟选择排序
+        {
+            index = i;
+            for (j = i + 1; j < n; j++)           //在无序区中查找最小记录
+                if (r[j] < r[index]) index = j;
+            if (index != i) {
+                temp = r[i]; r[i] = r[index]; r[index] = temp;
+            }//交换记录
+        }
+    }
+}
---
+++ src/edu/蛮力法/A6_起泡排序.java
@@ -0,0 +1,32 @@
+package edu.蛮力法;
+
+/**
+ * @Author WesterlyWindWuTong
+ * @Date 2023/1/10 01:40
+ * @Version 1.0
+ * @Description:
+ */
+public class A6_起泡排序 {
+    public static void main(String[] args) {
+        int[] r ={1,6,7,5,3,8,9,2};
+        BubbleSot(r,8);
+        for(int i=0;i<8;i++)
+            System.out.print(r[i]+"  ");
+        System.out.println();
+    }
+    private static void BubbleSot(int[] r, int n){
+        int bound, exchange = n-1;
+        while (exchange != 0){
+            bound=exchange;
+            exchange=0;
+            for (int j = 0; j<bound;j++){
+                if(r[j]>r[j+1]){
+                    int temp = r[j];
+                    r[j]=r[j+1];
+                    r[j+1]=temp;
+                    exchange = j;
+                }
+            }
+        }
+    }
+}
