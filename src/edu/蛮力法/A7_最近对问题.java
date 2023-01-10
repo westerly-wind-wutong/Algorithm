0d8cbd613df3231843142a12b68f1fa8b544e4cc
wutong <1772364946@qq.com>
2023-01-11 00:47:10 +0800

    蛮力法(brute force method)

---
+++ src/edu/蛮力法/A7_最近对问题.java
@@ -0,0 +1,31 @@
+package edu.蛮力法;
+
+/**
+ * @Author WesterlyWindWuTong
+ * @Date 2023/1/11 00:25
+ * @Version 1.0
+ * @Description:
+ */
+public class A7_最近对问题 {
+    public static void main(String[] args) {
+        int[] x = {1, 3, 5, 1};
+        int[] y = {1, 2, 4, 2};
+        ClosestPoints(x, y, 4);
+    }
+
+    private static void ClosestPoints(int[] x, int[] y, int n) {
+        int index1 = 0, index2 = 0;                           //记载最近点对的下标
+        int d, minDist = 1000;                        //假设最大距离不超过1000
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++)                  //只考虑i＜j的点对
+            {
+                d = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
+                if (d < minDist) {
+                    minDist = d;
+                    index1 = i;
+                    index2 = j;
+                }
+            }
+        System.out.println("最近的点对是：" + index1 + "和" + index2);
+    }
+}
---
+++ src/edu/蛮力法/A8_凸包问题.java
@@ -0,0 +1,36 @@
+package edu.蛮力法;
+
+/**
+ * @Author WesterlyWindWuTong
+ * @Date 2023/1/11 00:35
+ * @Version 1.0
+ * @Description:
+ */
+public class A8_凸包问题 {
+    public static void main(String[] args) {
+        int[] x ={1,3,5,1,2, 2,2};
+        int[] y ={1,1,4,3,2,3,6};
+        BulgePack(x,y,7);
+    }
+    private static void BulgePack(int[] x, int[] y, int n)
+    {
+        int i, j, k, sign1, sign2;
+        int a, b, c;
+        for (i = 0; i < n - 1; i++)
+            for (j = i + 1; j < n; j++)
+            {
+                sign1 = 0; sign2 = 0;
+                a = y[i] - y[j]; b = x[j] - x[i]; c = x[i] * y[j] - y[i] * x[j];
+                for (k = 0; k < n; k++)
+                {
+                    if (k != i && k != j) {
+                        if (a * x[k] + b * y[k] + c > 0) sign1 = 1;
+                        else sign2 = 1;
+                        if (sign1 == sign2) break;            //两个半平面均有点
+                    }
+                }
+                if (k == n)                            //点i和j是极点
+                    System.out.println("("+i+","+j+")");
+            }
+    }
+}
